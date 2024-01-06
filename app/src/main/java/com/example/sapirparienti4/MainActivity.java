389b4f3472f69f63a0d917e6e4d841a04536affd
sapir-parienti <sapir.pa@sisma.org.il>
2024-01-06 21:38:52 +0200

    3

--- app/src/main/java/com/example/sapirparienti4/MainActivity.java
+++ app/src/main/java/com/example/sapirparienti4/MainActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    Button btn;
+    int i=1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.button);
     }
 
     public void go(View view) {
-
+        String s="THIS IS CLICK NUMBER: "+i;
+        String s2="BOOM!";
+        btn.setText(s);
+        if(i%10==7||i%7==0)
+            btn.setText(s2);
+        i++;
     }
 }
